8af175a7da7ed709addcaf361667803e26903911
Nomimian01 <nomimian01@gmail.com>
2025-07-12 15:48:20 +0500

    Add MongoDB connection config

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+const urlRoutes = require('./routes/urlRoutes');
+const app = express();
+
+dotenv.config();
+connectDB();
+
+app.use(express.json());
+app.use('/api/shorten', urlRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
